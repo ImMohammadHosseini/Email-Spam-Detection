5a35fe309d5bb7425b159e5a9ca6bf5af9daa15b
ImMohammadHosseini <sayedmh50@gmail.com>
2023-04-06 17:17:38 +0330

    complete main

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 
+import pandas as pd
+import numpy as np
+from src.baysian import Baysian
+from src.ensemble import Ensemble
 
+if __name__ == "__main__":
+    data = pd.read_csv('data/emails.csv')
+
+
+    data= data.sample(frac=1)
+    msk = np.random.rand(len(data)) < 0.8
+    train = data[msk]
+    test = data[~msk]
+    #labels= list(data['Prediction'])
+
+    y_test = list(test['Prediction'])
+    test = test.drop(['Prediction', 'Email No.'], axis=1)
+
+    baysian = Baysian()
+    baysian.fit(data, train)
+    y_hat = baysian.test(test)
+    baysian.accuracy(y_test, y_hat)
+
+    ensembleModel = Ensemble(20, 100)
+    ensembleModel.fit(data)
+    y_hat= ensembleModel.test(60)
